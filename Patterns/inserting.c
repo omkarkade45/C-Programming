c1886aafa94a143a89a2809dd9f903c0055d0c78
Omkar <okakde8@gmail.com>
2026-07-30 19:47:54 +0530

    insertin.c is modified

--- Patterns/inserting.c
+++ Patterns/inserting.c
@@ -18,20 +18,42 @@
 //         printf("%d ",arr[j]);
 //     }
 // }
-int main(){
-    int arr[5]={1,2,3,4,5};
-    int num=0;
-    printf("Enter a Element :");
-    scanf("%d",&num);
-    int pos;
-    printf("Enter a position where you want to insert :");
-    scanf("%d",&pos);
-    for(int i=5;i>=pos;i--){
-        arr[i+1]=arr[i];
+#include<stdio.h>
+
+int main()
+{
+    int arr[6] = {1,2,3,4,5};
+    int size = 5;
+    int num, pos;
+
+    printf("Enter an element: ");
+    scanf("%d", &num);
+
+    printf("Enter position (0-5): ");
+    scanf("%d", &pos);
+
+    if(pos < 0 || pos > size)
+    {
+        printf("Invalid Position");
+        return 0;
     }
-    arr[pos]=num;
-     printf("\n Array elements :");
-     for(int j=0;j<6;j++){
-       printf("%d ",arr[j]);
+
+    // Shift elements to the right
+    for(int i = size; i > pos; i--)
+    {
+        arr[i] = arr[i-1];
     }
+
+    // Insert element
+    arr[pos] = num;
+    size++;
+
+    printf("\nArray Elements:\n");
+
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
 }
