6d227cd6c947f8707b7645aef747b4606d677503
Omkar <okakde8@gmail.com>
2026-08-09 22:56:05 +0530

    Insert At Last

---
+++ DSA/LinkedList/InsertLast.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+struct  Node{
+    int data;
+    struct Node *next;
+};
+struct Node *head=NULL;
+void insert(){
+      int *newnode,*temp;
+      newnode=(struct Node *)malloc
+}
+
